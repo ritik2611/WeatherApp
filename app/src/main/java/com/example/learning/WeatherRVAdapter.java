48267201c9c166b24df3d0f1bf8192950563e955
Ritik Kumar Pandey <ritikpandey2610@gmail.com>
2024-01-01 18:03:26 +0530

    Created an weather app.

---
+++ app/src/main/java/com/example/learning/WeatherRVAdapter.java
@@ -0,0 +1,71 @@
+package com.example.learning;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class WeatherRVAdapter extends RecyclerView.Adapter<WeatherRVAdapter.ViewHolder> {
+    private Context context;
+    private ArrayList<WeatherRVModal> weatherRVModalArrayList;
+
+    public WeatherRVAdapter(Context context, ArrayList<WeatherRVModal> weatherRVModalArrayList) {
+        this.context = context;
+        this.weatherRVModalArrayList = weatherRVModalArrayList;
+    }
+
+    @NonNull
+    @Override
+    public WeatherRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.weather_rv,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WeatherRVAdapter.ViewHolder holder, int position) {
+
+        WeatherRVModal modal = weatherRVModalArrayList.get(position);
+        Picasso.get().load("https:".concat(modal.getIcon())).into(holder.conditionIV);
+        holder.temperatureTV.setText(modal.getTemperature()+"°c");
+        holder.windTv.setText(modal.getWindSpeed()+"Km/hr");
+        SimpleDateFormat input = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+        SimpleDateFormat output = new SimpleDateFormat("hh:mm aa");
+        try {
+            Date t = input.parse(modal.getTime());
+            holder.timeTV.setText(output.format(t));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return weatherRVModalArrayList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView windTv,temperatureTV,timeTV;
+        private ImageView conditionIV;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            windTv = itemView.findViewById(R.id.idTVWindSpeed);
+            temperatureTV = itemView.findViewById(R.id.idTVTemperatureCard);
+            timeTV =itemView.findViewById(R.id.idTVTime);
+            conditionIV = itemView.findViewById(R.id.idIVCondition);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/learning/WeatherRVModal.java
@@ -0,0 +1,48 @@
+package com.example.learning;
+
+public class WeatherRVModal {
+
+    private  String time;
+    private String temperature;
+    private String icon;
+    private String windSpeed;
+
+    public WeatherRVModal(String windSpeed,String time,String temperature,String icon) {
+        this.windSpeed = windSpeed;
+        this.time = time;
+        this.icon = icon;
+        this.temperature = temperature;
+    }
+
+    public String getWindSpeed() {
+        return windSpeed;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public void setWindSpeed(String windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(String temperature) {
+        this.temperature = temperature;
+    }
+}
